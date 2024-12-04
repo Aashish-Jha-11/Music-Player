8e56fde11e57a27c226dd299e159f455ca8d79d9
Aashish Jha <aashishjha1107@gmail.com>
2024-12-05 00:48:03 +0530

    script.js
    Making arr

---
+++ script.js
@@ -0,0 +1,6 @@
+var arr = [
+    { songName: "Sankat Mochan Hanuman", url: "Songs/Sankat Mochan Hanuman.mp3", img: "Thumbnails/Sankat-Mochan-Hanuman.jpg" },
+    { songName: "Unstoppable", url: "Songs/Unstoppable.mp3", img: "Thumbnails/Unstoppable.jpg" },
+    { songName: "Last Breath ", url: "Songs/Last Breath.mp3", img: "Thumbnails/Last_Breath.jpg" },
+    { songName: "Make Some Noise For Desi Boyz ", url: "Songs/Make Some Noise For Desi Boyz.mp3", img: "Thumbnails/Desi-Boyz.jpg" }
+]
